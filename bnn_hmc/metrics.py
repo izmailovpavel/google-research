da9b75d63c69202392bbcf6c096c94b6257a3e7c
Pavel Izmailov <izmailovpavel@gmail.com>
2020-11-12 03:21:14 +0000

    Metrics

---
+++ bnn_hmc/metrics.py
@@ -0,0 +1,66 @@
+"""numpy implementations of prediction quality metrics.
+
+Partly adapted from https://github.com/wjmaddox/swa_gaussian/blob/master/experiments/uncertainty/uncertainty.py
+"""
+
+import numpy as onp
+
+
+def accuracy(outputs, labels):
+  """Negative log-likelihood."""
+  labels = labels.astype(int)
+  preds = onp.argmax(outputs, axis=1)
+  return (preds == labels).mean()
+
+
+def nll(outputs, labels, normalized=True):
+  """Negative log-likelihood."""
+  labels = labels.astype(int)
+  idx = (onp.arange(labels.size), labels)
+  log_ps = onp.log(outputs[idx])
+  if normalized:
+    return -log_ps.mean()
+  else:
+    return -log_ps.sum()
+
+
+def calibration_curve(outputs, labels, num_bins=20):
+  """Compute calibration curve and ECE."""
+
+  confidences = onp.max(outputs, 1)
+  num_inputs = confidences.shape[0]
+  step = (num_inputs + num_bins - 1) // num_bins
+  bins = onp.sort(confidences)[::step]
+  if num_inputs % step != 1:
+    bins = onp.concatenate((bins, [onp.max(confidences)]))
+  predictions = onp.argmax(outputs, 1)
+  bin_lowers = bins[:-1]
+  bin_uppers = bins[1:]
+
+  accuracies = (predictions == labels)
+
+  bin_confidences = []
+  bin_accuracies = []
+  bin_proportions = []
+
+  ece = 0.0
+  for bin_lower, bin_upper in zip(bin_lowers, bin_uppers):
+    in_bin = (confidences > bin_lower) * (confidences < bin_upper)
+    prop_in_bin = in_bin.mean()
+    if prop_in_bin > 0:
+      accuracy_in_bin = accuracies[in_bin].mean()
+      avg_confidence_in_bin = confidences[in_bin].mean()
+      ece += onp.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+      bin_confidences.append(avg_confidence_in_bin)
+      bin_accuracies.append(accuracy_in_bin)
+      bin_proportions.append(prop_in_bin)
+
+  bin_confidences, bin_accuracies, bin_proportions = map(
+      lambda lst: onp.array(lst),
+      (bin_confidences, bin_accuracies, bin_proportions))
+  
+  return {
+      "confidence": bin_confidences,
+      "accuracy": bin_accuracies,
+      "proportions": bin_proportions,
+      "ece": ece}
